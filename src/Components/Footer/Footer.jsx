144823c123e296c34198fcf7ff71ec85d3854fb5
4alaa <shammsalaa628@gmail.com>
2024-04-26 19:53:34 +0200

    first commit

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export default function Footer() {
+  return <>
+  <footer>
+
+
+
+
+  <footer className="footer" >
+	<div className="waves">
+		<div className="wave" id="wave1"></div>
+		<div className="wave" id="wave2"></div>
+		<div className="wave" id="wave3"></div>
+		<div className="wave" id="wave4"></div>
+	</div>
+
+	<ul className="menu">
+
+
+
+	</ul>
+	<p className="fs-4 fw-bolder m-0 p-0">( 🤍 Made By  ) </p>
+
+	<p className="fs-4 fw-bolder text-center m-0 p-0">Eng Alaa Shamms    </p>
+
+
+</footer>
+
+
+  </footer>
+
+  </>
+}
